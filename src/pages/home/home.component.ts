251b24963f89801410a81c1fd9d8eea632ba9a47
melaniemdm <melanieborja.pro@gmail.com>
2023-10-09 22:31:32 +0200

    feature(ts) securisation de la page grace au token

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private cookieService: CookieService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.checkToken();
+  }
+
+  checkToken(): void {
+    const token = this.cookieService.get('sessionToken');
+
+    if (!token) {
+      this.router.navigate(['/login']);
+    }
+
+    // Vous pouvez également effectuer des validations supplémentaires sur le token ici si nécessaire.
+  }
 }
