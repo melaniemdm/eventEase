0a6f8bfba682ab1cd232f37d5352db0075e1784f
melaniemdm <melanieborja.pro@gmail.com>
2023-10-09 22:28:17 +0200

    fix(ts) connexion backend login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { CookieService } from 'ngx-cookie-service';  // Importez le CookieService
+import { Router } from '@angular/router'; // Importez le service Router
 
 @Component({
   selector: 'app-login',
@@ -10,18 +12,30 @@ export class LoginComponent {
   user: any = {};
   errorMessage: string = ''; // Pour afficher les messages d'erreur
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, 
+    private cookieService: CookieService, 
+    private router: Router ) { }
 
-  async onSubmit() {
-    try {
-      const response = await this.apiService.getToken(this.user.email, this.user.password);
-      // Gestion de la réussite de la connexion ici
-      console.log('Réponse réussie :', response);
-      this.errorMessage = ''; // Réinitialisez le message d'erreur en cas de succès
-    } catch (error) {
-      // Gestion de l'erreur de connexion ici
-      console.error('Erreur de connexion :', error);
-      this.errorMessage = 'Erreur de connexion. Veuillez vérifier vos informations.';
-    }
+  onSubmit() {
+    this.apiService.getToken(this.user.email, this.user.password).subscribe(
+      response => {
+        // Gestion de la réussite de la connexion ici
+        console.log('Réponse réussie :', response);
+        
+        // Si l'API renvoie un token, enregistrez-le dans un cookie
+        if (response && response.sessionToken) {
+          this.cookieService.set('sessionToken', response.sessionToken);
+           // Naviguer vers la route racine
+           this.router.navigate(['/']);
+        }
+
+        this.errorMessage = ''; // Réinitialisez le message d'erreur en cas de succès
+      },
+      error => {
+        // Gestion de l'erreur de connexion ici
+        console.error('Erreur de connexion :', error);
+        this.errorMessage = 'Erreur de connexion. Veuillez vérifier vos informations.';
+      }
+    );
   }
 }
