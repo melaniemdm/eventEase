a3c032a0bb2373c48e69d6c19c4779897500a9f7
melaniemdm <melanieborja.pro@gmail.com>
2023-10-10 11:59:45 +0200

    fix(ts & html) affichage firstName

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service'; // Assurez-vous que le chemin est correct
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  firstName: string | null = null;
 
+  constructor(
+    private apiService: ApiService,
+    private cookieService: CookieService
+  ) {}
+
+  ngOnInit(): void {
+    const token = this.cookieService.get('sessionToken');
+     // Affichage du token dans la console
+     console.log('sessionToken', token);
+    if (token) {
+      this.apiService.getUser(token).subscribe(
+        response => {
+          this.firstName = response.firstName;
+          console.log(this.firstName);
+        },
+        error => {
+          console.error('Failed to get username', error);
+        }
+      );
+    }
+  }
 }
