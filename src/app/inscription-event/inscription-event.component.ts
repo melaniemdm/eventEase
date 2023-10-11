e1f8dd64a3e0725db1a20cccdbbd23e232a4170f
melaniemdm <melanieborja.pro@gmail.com>
2023-10-11 10:45:08 +0200

    fix(ts & html) recup données du form

--- src/app/inscription-event/inscription-event.component.ts
+++ src/app/inscription-event/inscription-event.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+interface Participant {
+  event: string;
+  date: string;
+  timeStart?: string;
+  timeEnd?: string;
+}
 
 @Component({
   selector: 'app-inscription-event',
   templateUrl: './inscription-event.component.html',
   styleUrls: ['./inscription-event.component.scss']
 })
-export class InscriptionEventComponent {
-  participant: any = {};
+export class InscriptionEventComponent implements OnInit {
+  participantForm!: FormGroup;
+  
+  ngOnInit() {
+    this.participantForm = new FormGroup({
+      event: new FormControl('', Validators.required),
+      date: new FormControl('', Validators.required),
+      timeStart: new FormControl(''),
+      timeEnd: new FormControl('')
+    });
+    
+  }
 
   onSubmit() {
-    // Logique de traitement du formulaire ici (par exemple, envoi des données au serveur).
-    console.log(this.participant);
-  }
+    // recuperation des données du formulaire
+    console.log('Statut du Formulaire:', this.participantForm.status);
+  console.log('Valeurs du Formulaire:', this.participantForm.value);
+  
+  if (this.participantForm.valid) {
+    const participant: Participant = this.participantForm.value;
+    console.log(participant);
+  } else {
+    console.log('Formulaire non valide');
+  }}
+
+  
 }
