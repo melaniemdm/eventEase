4d68bcb4d50b17e05b5584a865d7bd436bf159f0
melaniemdm <melanieborja.pro@gmail.com>
2023-10-13 13:43:22 +0200

    fix(ts) affichage des events sur le calendar

--- src/pages/calendar/calendar.component.ts
+++ src/pages/calendar/calendar.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CalendarEvent, CalendarView } from 'angular-calendar';
 import { isSameDay, isSameMonth } from 'date-fns';
+import { CookieService } from 'ngx-cookie-service';
+import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit{
 viewDate:Date = new Date();
 view: CalendarView= CalendarView.Week;
 CalendarView=CalendarView;
@@ -14,7 +16,8 @@ CalendarView=CalendarView;
 events:CalendarEvent[] = [];
 
 activeDayIsOpen = false;
-constructor(){
+constructor( private cookieService: CookieService,
+  private apiService: ApiService){
   const event1 = {
     title:"Mélanie",
     start:new Date("2023-10-09T10:30"),
@@ -26,6 +29,45 @@ constructor(){
     end:new Date("2023-10-09T12:00"),
   }
   this.events.push(event1,event2)
+}
+ngOnInit(): void {
+  this.loadsEventsCalendar();
+}
+
+loadsEventsCalendar() {
+  //recuperation du token 
+  const token = this.cookieService.get('sessionToken');
+  console.log('token', token);
+  //recuperation des events dans le backend
+  this.apiService.getEvents(token).subscribe(
+    (data: any) => {
+      console.log('Data reçue:', data);
+      console.log(JSON.stringify(data));
+      // Assurez-vous que chaque objet a des propriétés `start` et `title`.
+      this.events = data.map((event: any) => {
+        const eventDate = new Date(event.date);
+
+        // Créer un nouvel objet Date pour le début (start) de l'événement à 00:01
+        const start = new Date(eventDate);
+        start.setHours(8, 0);  // heure, minute
+      
+        // Créer un nouvel objet Date pour la fin (end) de l'événement à 23:59
+        const end = new Date(eventDate);
+        end.setHours(12, 0);  // heure, minute
+      
+        return {
+          title: event.titleEvent,  // utiliser la propriété 'titleEvent' pour le titre
+          start: start,  // utiliser l'objet Date de début modifié
+          end: end,  // utiliser l'objet Date de fin modifié
+          // ... ajoutez ici d'autres propriétés si besoin ...
+        };
+      });
+      console.log(this.events);
+    },
+    (error) => {
+      console.error('Erreur lors de la récupération des événements:', error);
+    }
+  );
 }
 
 setView(view: CalendarView){
