2818ae59291c6c29c5a292bfa841f2b538450477
melaniemdm <melanieborja.pro@gmail.com>
2023-10-18 15:05:11 +0200

    refacto

--- src/pages/calendar/calendar.component.ts
+++ src/pages/calendar/calendar.component.ts
@@ -46,117 +46,61 @@ export class CalendarComponent implements OnInit {
     //recuperation des events dans le backend
     this.apiService.getEvents(token).subscribe(
       (data: any) => {
-        console.log('Data reçue:', data);
-        //console.log(JSON.stringify(data));
-        // Assurez-vous que chaque objet a des propriétés `start` et `title`.
         this.events = data.map((event: any) => {
           const eventDate = new Date(event.date + 'Z');
-          console.log(eventDate);
-
-
-
-          //modification du start
-          const yourDateStart = new Date(event.date + 'Z');
-          yourDateStart.setHours(8, 0, 0, 0);
-          let start = new Date(yourDateStart);
-
-          let hours = yourDateStart.getHours();
-          let minutes = yourDateStart.getMinutes();
-          // Si vous voulez afficher l'heure sous la forme "08:00" avec les zéros au début pour les heures ou les minutes inférieures à 10
-          let timeStringStart = (hours).toString().padStart(2, '0') + ':' +
-            (minutes).toString().padStart(2, '0');
-          console.log(timeStringStart); // affiche 8:00
-
-          //modification du end
-          const yourDateEnd = new Date(event.date + 'Z');
-          yourDateEnd.setHours(12, 0, 0, 0);
-          console.log(yourDateEnd);
-          let end = new Date(yourDateEnd + 'Z');
-          let hoursEnd = yourDateEnd.getHours();
-          let minutesEnd = yourDateEnd.getMinutes();
-
-          // Si vous voulez afficher l'heure sous la forme "12:00" avec les zéros au début pour les heures ou les minutes inférieures à 10
-          let timeStringEnd = (hoursEnd).toString().padStart(2, '0') + ':' +
-            (minutesEnd).toString().padStart(2, '0');
-          console.log(timeStringEnd); // affiche 12:00
-
+    
+          // Initialisation de start et end
+          let start = new Date(event.date + 'Z');
+          start.setHours(8, 0, 0, 0);
+    
+          let end = new Date(event.date + 'Z');
+          end.setHours(12, 0, 0, 0);
+    
           if (event.titleEvent === 'Collecte alimentaire') {
-            // créer les objets Date pour le début et la fin
-            start = new Date(yourDateStart);
-            const [heureStart, minuteStart] = event.participant[0].heureStart.split(':');
-            start.setHours(heureStart, minuteStart);  // heure, minute
-            end = new Date(yourDateStart);
-            const [heureEnd, minuteEnd] = event.participant[0].heureEnd.split(':');
-            end.setHours(heureEnd, minuteEnd);  // heure, minute
-
-
-
-            // Pour chaque participant
             event.participant.forEach((participant: any) => {
-
-              // Assurez-vous que les heures sont définies
               if (participant.heureStart && participant.heureEnd) {
-                let participantStart = new Date(yourDateStart);
+                const participantStart = new Date(eventDate);
                 const [heureStart, minuteStart] = participant.heureStart.split(':');
-                participantStart.setHours(heureStart, minuteStart);  // heure, minute
-                let participantEnd = new Date(yourDateStart);
+                participantStart.setHours(+heureStart, +minuteStart);
+    
+                const participantEnd = new Date(eventDate);
                 const [heureEnd, minuteEnd] = participant.heureEnd.split(':');
-                participantEnd.setHours(heureEnd, minuteEnd);  // heure, minute
-                // console.log("toto");
-                // console.log('participantStart', participantStart);
-                // console.log('participationEnd', participantStart);
-                // console.log('start', start);
-                // console.log('end', end);
-
-
-
-
-                // Pour chaque participant, vérifier si heureStart est antérieure à start
+                participantEnd.setHours(+heureEnd, +minuteEnd);
+    
                 if (participantStart < start) {
-                  // remplacer start par heureStart du participant
-                  start = new Date(participantStart);
+                  start = participantStart;
                 }
-                // pour chaque participant, vérifier si heureEnd est supérieure à end
                 if (participantEnd > end) {
-                  end = new Date(participantEnd);
+                  end = participantEnd;
                 }
-
-
               }
             });
           }
-          // Construisez une chaîne avec les noms des participants
-          let participantsStr = '';
-         
-          if (event.participant && event.participant.length > 0) {
-            const participantNames = event.participant.map((participant: Participant) => {
-              let firstName = participant.firstName;
+    
+          const participantsStr = event.participant && event.participant.length > 0 ? 
+            ` (Nombre de participant :${event.participant.length}) - ${event.participant.map((participant: any) => {
+              let name = participant.firstName;
               if (participant.heureStart && participant.heureEnd) {
-                firstName += ' (' + participant.heureStart + '-' + participant.heureEnd + ')';
+                name += ` (${participant.heureStart}-${participant.heureEnd})`;
               }
-              return firstName
-            });
-            participantsStr = ` (Nombre de participant :${participantNames.length}) - ${participantNames.join(', ')}`;
-           
-          }
-          const newEvent = {
-            title: `${event.titleEvent}${participantsStr} `, // utiliser la propriété 'titleEvent' pour le titre
-            start: start,  // utiliser l'objet Date de début modifié
-            end: end,  // utiliser l'objet Date de fin modifié
-            type: event.typeEvent 
+              return name;
+            }).join(', ')}` : '';
+    
+          return {
+            title: `${event.titleEvent}${participantsStr}`,
+            start: start,
+            end: end,
+            type: event.typeEvent
           };
-          console.log(newEvent);
-
-
-          return newEvent
         });
-
+    
         console.log(this.events);
       },
       (error) => {
         console.error('Erreur lors de la récupération des événements:', error);
       }
     );
+    
   }
 //au click envoie sur la page d'inscription
 eventClicked({ event }: { event: ExtendedCalendarEvent }): void {
